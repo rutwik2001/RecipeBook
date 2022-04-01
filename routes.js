d2520a118950d4c9a737c71cabe2b95b040d0cfd
Pasumarthi Rutwik <62496029+rutwik2001@users.noreply.github.com>
2022-04-01 18:49:35 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const routes = require('next-routes-extended')
 
 module.exports = routes()
-.add('connect', '/connect', '/connect')
 .add('profile', '/creators/:address', '/creators/profile')
 .add('createRecipe','/creators/:address/createRecipe', '/creators/createRecipe')
 .add('showRecipe', '/creators/showRecipe/:tokenID', '/creators/showRecipe')
